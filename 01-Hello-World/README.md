858554c8b55e4a7fd6d5cf42789b70a0a4e350e9
Kevin Balmores <balmoresk3v1n@gmail.com>
2023-06-07 18:14:45 +0800

    1-100 React Projects

---
+++ 01-Hello-World/README.md
@@ -0,0 +1,3 @@
+# 01-Hello-World
+
+Create a simple React app that displays "Hello, World!" on the screen.
---
+++ 01-Hello-World/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='d-flex justify-content-center h-full'>
+      <h1>Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App

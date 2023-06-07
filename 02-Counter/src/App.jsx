f9dc864d2e6b8b630fbce35957338b3560482fd2
Kevin Balmores <balmoresk3v1n@gmail.com>
2023-06-07 18:52:20 +0800

    2-100 React Projects

---
+++ 02-Counter/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { FaMinus, FaPlus } from 'react-icons/fa'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+
+  const plusClick = () => {
+    let newCount = count + 1
+    setCount(newCount)
+  }
+
+  const minusClick = () => {
+    let newCount = count - 1
+    if (newCount < 0) {
+      newCount = 0 
+    }
+    setCount(newCount)
+  }
+
+  return (
+    <>
+    <h1 className='text-4xl text'>Counter</h1>
+    <div className='flex justify-center text-5xl gap-6 mt-10'>
+        <button
+          type="button"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+          onClick={minusClick}
+          className="inline-block rounded bg-blue-700 text-white px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+          <FaMinus size={25} />
+        </button>
+        <h1 >{count}</h1>
+        <button
+          type="button"
+          data-te-ripple-init
+          data-te-ripple-color="light"
+          onClick={plusClick}
+          className="inline-block rounded bg-blue-700 text-white px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+          <FaPlus size={25} />
+        </button>
+    </div>
+    </>
+  )
+}
+
+export default App

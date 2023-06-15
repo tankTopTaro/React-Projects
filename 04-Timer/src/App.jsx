4065f98df96453ff72d22893ad2d1f6512054f7a
Kevin Balmores <balmoresk3v1n@gmail.com>
2023-06-16 06:40:43 +0800

    04-Timer | set up counters for time

--- 04-Timer/src/App.jsx
+++ 04-Timer/src/App.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react"
 import Timepicker from "./components/Timepicker"
 
 function App() {
+  const [hour, setHour] = useState(0)
+  const [minute, setMinute] = useState(0)
+  const [second, setSecond] = useState(0)
+
+  const time = (t) => {
+    let c = [...Array(t).keys()]
+    return c
+  }
+
+  const hours = time(24)
+  const minutes = time(60)
+  const seconds = time(60)
+
+  useEffect(() => {
+    if (hour === 0 && minute === 0) {
+      setSecond(1)
+    } 
+  }, [])
 
   return (
     <>
       <div className="flex items-center justify-center w-screen h-screen">
-        <Timepicker /> <span className="text-4xl px-2">:</span> <Timepicker /> <span className="text-4xl px-2">:</span> <Timepicker />
+        <Timepicker time={hours} count={hour} setCount={setHour} /> 
+        <span className="text-4xl px-2">:</span> 
+        <Timepicker time={minutes} count={minute} setCount={setMinute} /> 
+        <span className="text-4xl px-2">:</span> 
+        <Timepicker time={seconds} count={second} setCount={setSecond} />
       </div>
     </>
   )
--- 04-Timer/src/components/Timepicker.jsx
+++ 04-Timer/src/components/Timepicker.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/24/outline"
 
-const Timepicker = () => {
+const Timepicker = ({ time, count, setCount }) => {
+    const formattedTime = parseInt(count).toString().padStart(2, '0')
+
+    const increaseTime = (maxTime) => {
+        let t = count + 1
+        if (t >= maxTime.length) {
+          t = 0
+        }
+        setCount(t)
+    }
+
     return (
         <div className="flex-col w-20 h-20">
-            <div className="flex items-center justify-center"><ChevronUpIcon className="h-6 w-6 block"/></div>
-            <div className="border-solid border-black border-2 text-4xl text-center flex items-center justify-center">
-                00
+            <div className="flex items-center justify-center">
+                <button 
+                    type='button' 
+                    onClick={() => increaseTime(time)}>
+                    <ChevronUpIcon className="h-6 w-6 block"/>
+                </button>
             </div>
-            <div className="flex items-center justify-center"><ChevronDownIcon className="h-6 w-6 block"/></div>
+            <div className="border-solid border-black border-2 text-4xl text-center flex items-center justify-center">
+                {formattedTime}
+            </div>
+            <div className="flex items-center justify-center">
+                <button 
+                    type='button' 
+                    >
+                    <ChevronDownIcon className="h-6 w-6 block"/>
+                </button>
+            </div>
         </div>
     )
 }
 
 export default Timepicker
+
